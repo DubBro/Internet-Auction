6c8bc64a18c880191a52350988879f0235978fe2
Krimyllex <taras.melnyk.aka.krimyllex@gmail.com>
2022-06-02 05:40:23 +0300

    BLL unit tests implemented

---
+++ BLLTests/Services/AuctionServiceTests.cs
@@ -0,0 +1,423 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AutoFixture;
+using BLL.Interfaces;
+using DAL.Interfaces;
+using AutoMapper;
+using NSubstitute;
+using BLL.Infrastructure;
+using BLL.DTOs;
+using BLL.Exceptions;
+using DAL.Entities;
+using BLL.Services;
+
+namespace BLLTests.Services
+{
+    [TestFixture]
+    public class AuctionServiceTests
+    {
+        private readonly IFixture fixture = new Fixture();
+
+        private IAuctionService service;
+        private IUnitOfWork uow;
+        private IMapper mapper;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            uow = Substitute.For<IUnitOfWork>();
+            fixture.Inject(uow);
+
+            mapper = AutoMapperBLL.InitializeAutoMapper();
+
+            service = fixture.Create<AuctionService>();
+        }
+
+        [Test]
+        public void Bet_should_return_exception_if_id_is_less_or_equals_0()
+        {
+            // Arrange
+            int id = -1;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.Bet(id, name, bid); });
+        }
+
+        [Test]
+        public void Bet_should_return_exception_if_customer_name_equals_null()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = null;
+            int bid = fixture.Create<int>();
+
+            // Act and assert
+            Assert.Throws<InvalidNameException>(delegate { service.Bet(id, name, bid); });
+        }
+
+        [Test]
+        public void Bet_should_return_exception_if_auction_not_exist_in_db()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns((Auction)null);
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.Bet(id, name, bid); });
+        }
+
+        [Test]
+        public void Bet_should_return_exception_if_auction_is_not_started()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+            Auction auction = new Auction { Started = false, Ended = false};
+            uow.Auctions.Get(id).Returns(auction);
+
+            // Act and assert
+            Assert.Throws<InvalidAuctionException>(delegate { service.Bet(id, name, bid); });
+        }
+
+        [Test]
+        public void Bet_should_return_exception_if_auction_is_finished()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+            Auction auction = new Auction { Started = false, Ended = true };
+            uow.Auctions.Get(id).Returns(auction);
+
+            // Act and assert
+            Assert.Throws<InvalidAuctionException>(delegate { service.Bet(id, name, bid); });
+        }
+
+        [Test]
+        public void Bet_should_return_exception_if_bid_is_low()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+            Auction auction = new Auction { Started = false, Ended = true, Bid = bid + 10 };
+            uow.Auctions.Get(id).Returns(auction);
+
+            // Act and assert
+            Assert.Throws<InvalidAuctionException>(delegate { service.Bet(id, name, bid); });
+        }
+
+        [Test]
+        public void Bet_should_call_auction_repository_get_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = false, Bid = bid - 10 });
+
+            // Act
+            service.Bet(id, name, bid);
+
+            // Assert
+            uow.Auctions.Received(1).Get(Arg.Any<int>());
+        }
+
+        [Test]
+        public void Bet_should_call_auction_repository_update_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = false, Bid = bid - 10 });
+
+            // Act
+            service.Bet(id, name, bid);
+
+            // Assert
+            uow.Auctions.Received(1).Update(Arg.Any<Auction>());
+        }
+
+        [Test]
+        public void Bet_should_call_unit_of_work_commit_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>(); ;
+            string name = fixture.Create<string>();
+            int bid = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = false, Bid = bid - 10 });
+
+            // Act
+            service.Bet(id, name, bid);
+
+            // Assert
+            uow.Received(1).Commit();
+        }
+
+        [Test]
+        public void CloseAuction_should_return_exception_if_id_is_less_or_equals_0()
+        {
+            // Arrange
+            int id = -1;
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.CloseAuction(id); });
+        }
+
+        [Test]
+        public void CloseAuction_should_return_exception_if_auction_not_exist_in_db()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns((Auction)null);
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.CloseAuction(id); });
+        }
+
+        [Test]
+        public void CloseAuction_should_return_exception_if_auction_is_not_started()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = false, Ended = false });
+
+            // Act and assert
+            Assert.Throws<InvalidAuctionException>(delegate { service.CloseAuction(id); });
+        }
+
+        [Test]
+        public void CloseAuction_should_return_exception_if_auction_is_finished()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = true });
+
+            // Act and assert
+            Assert.Throws<InvalidAuctionException>(delegate { service.CloseAuction(id); });
+        }
+
+        [Test]
+        public void CloseAuction_should_call_lot_repository_update_method_once_if_auction_leader_is_not_null()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = false, Leader = "a", Lot = new Lot() });
+
+            // Act
+            service.CloseAuction(id);
+
+            // Assert
+            uow.Lots.Received(1).Update(Arg.Any<Lot>());
+        }
+
+        [Test]
+        public void CloseAuction_should_call_auction_repository_update_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = false });
+
+            // Act
+            service.CloseAuction(id);
+
+            // Assert
+            uow.Auctions.Received(1).Update(Arg.Any<Auction>());
+        }
+
+        [Test]
+        public void CloseAuction_should_call_unit_of_work_commit_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = false });
+
+            // Act
+            service.CloseAuction(id);
+
+            // Assert
+            uow.Received(1).Commit();
+        }
+
+        [Test]
+        public void CloseAuction_should_call_auction_repository_get_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true, Ended = false });
+
+            // Act
+            service.CloseAuction(id);
+
+            // Assert
+            uow.Auctions.Received(1).Get(Arg.Any<int>());
+        }
+
+        [Test]
+        public void GetAuction_should_return_exception_if_id_is_less_or_equals_0()
+        {
+            // Arrange
+            int id = -1;
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.GetAuction(id); });
+        }
+
+        [Test]
+        public void GetAuction_should_return_exception_if_auction_not_exist_in_db()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns((Auction)null);
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.GetAuction(id); });
+        }
+
+        [Test]
+        public void GetAuction_should_return_AuctionDTO()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            Auction auction = new Auction();
+            AuctionDTO auctionDTO = mapper.Map<Auction, AuctionDTO>(auction);
+            uow.Auctions.Get(id).Returns(auction);
+
+            // Act
+            var testAuctionDTO = service.GetAuction(id);
+
+            // Assert
+            Assert.IsTrue(testAuctionDTO.GetType() == auctionDTO.GetType());
+        }
+
+        [Test]
+        public void GetAuction_should_call_auction_reository_get_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction());
+
+            // Act
+            service.GetAuction(id);
+
+            // Assert
+            uow.Auctions.Received(1).Get(Arg.Any<int>());
+        }
+
+        [Test]
+        public void GetAuctions_should_call_auction_repository_get_all_method_once()
+        {
+            // Arrange
+
+            // Act
+            service.GetAuctions();
+
+            // Assert
+            uow.Auctions.Received(1).GetAll();
+        }
+
+        [Test]
+        public void GetAuctions_should_return_IEnumerable_AuctionDTO()
+        {
+            // Arrange
+            IEnumerable<AuctionDTO> auctionDTOs = new List<AuctionDTO>();
+
+            // Act
+            var testAuctionDTOs = service.GetAuctions();
+
+            // Assert
+            Assert.IsTrue(testAuctionDTOs.GetType() == auctionDTOs.GetType());
+        }
+
+        [Test]
+        public void OpenAuction_should_return_exception_if_id_is_less_or_equals_0()
+        {
+            // Arrange
+            int id = -1;
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.OpenAuction(id); });
+        }
+
+        [Test]
+        public void OpenAuction_should_return_exception_if_auction_not_exist_in_db()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns((Auction)null);
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.OpenAuction(id); });
+        }
+
+        [Test]
+        public void OpenAuction_should_return_exception_if_auction_is_started()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Started = true });
+
+            // Act and assert
+            Assert.Throws<InvalidAuctionException>(delegate { service.OpenAuction(id); });
+        }
+
+        [Test]
+        public void OpenAuction_should_return_exception_if_auction_is_finished()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction { Ended = true });
+
+            // Act and assert
+            Assert.Throws<InvalidAuctionException>(delegate { service.OpenAuction(id); });
+        }
+
+        [Test]
+        public void OpenAuction_should_call_auction_repository_get_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction());
+
+            // Act
+            service.OpenAuction(id);
+
+            // Assert
+            uow.Auctions.Received(1).Get(Arg.Any<int>());
+        }
+
+        [Test]
+        public void OpenAuction_should_call_auction_repository_update_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction());
+
+            // Act
+            service.OpenAuction(id);
+
+            // Assert
+            uow.Auctions.Received(1).Update(Arg.Any<Auction>());
+        }
+
+        [Test]
+        public void OpenAuction_should_call_unit_of_work_commit_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Auctions.Get(id).Returns(new Auction());
+
+            // Act
+            service.OpenAuction(id);
+
+            // Assert
+            uow.Received(1).Commit();
+        }
+    }
+}
---
+++ BLLTests/Services/LotServiceTests.cs
@@ -0,0 +1,425 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using AutoFixture;
+using BLL.Interfaces;
+using DAL.Interfaces;
+using AutoMapper;
+using NSubstitute;
+using BLL.Infrastructure;
+using BLL.DTOs;
+using BLL.Exceptions;
+using DAL.Entities;
+
+namespace BLL.Services.Tests
+{
+    [TestFixture]
+    public class LotServiceTests
+    {
+        private readonly IFixture fixture = new Fixture();
+
+        private ILotService service;
+        private IUnitOfWork uow;
+        private IMapper mapper;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            uow = Substitute.For<IUnitOfWork>();
+            fixture.Inject(uow);
+
+            mapper = AutoMapperBLL.InitializeAutoMapper();
+
+            service = fixture.Create<LotService>();
+        }
+
+        [Test]
+        public void AddLot_should_return_exception_if_lot_equals_null()
+        {
+            // Arrange
+            LotDTO lot = null;
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.AddLot(lot); });
+        }
+
+        [Test]
+        public void AddLot_should_return_exception_if_lot_name_equals_null()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Name = null };
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.AddLot(lot); });
+        }
+
+        [Test]
+        public void AddLot_should_return_exception_if_lot_owner_equals_null()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Owner = null };
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.AddLot(lot); });
+        }
+
+        [Test]
+        public void AddLot_should_return_exception_if_lot_category_equals_null()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Category = null };
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.AddLot(lot); });
+        }
+
+        [Test]
+        public void AddLot_should_return_exception_if_lot_is_sold()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Sold = true };
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.AddLot(lot); });
+        }
+
+        [Test]
+        public void AddLot_should_call_lot_repository_add_method_once()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Name = "a", Category = "b", Owner = "c"};
+            
+            // Act
+            service.AddLot(lot);
+
+            // Assert
+            uow.Lots.Received(1).Add(Arg.Any<Lot>());
+        }
+
+        [Test]
+        public void AddLot_should_call_unit_of_work_commit_method_once()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Name = "a", Category = "b", Owner = "c"};
+
+            // Act
+            service.AddLot(lot);
+
+            // Assert
+            uow.Received(1).Commit();
+        }
+
+        [Test]
+        public void DeleteLot_should_return_exception_if_id_is_less_or_equals_0()
+        {
+            // Arrange
+            int id = -1;
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.DeleteLot(id); });
+        }
+
+        [Test]
+        public void DeleteLot_should_call_auction_repository_delete_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+
+            // Act
+            service.DeleteLot(id);
+
+            // Assert
+            uow.Auctions.Received(1).Delete(Arg.Any<int>());
+        }
+
+        [Test]
+        public void DeleteLot_should_call_lot_repository_delete_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+
+            // Act
+            service.DeleteLot(id);
+
+            // Assert
+            uow.Lots.Received(1).Delete(Arg.Any<int>());
+        }
+
+        [Test]
+        public void DeleteLot_should_call_unit_of_work_commit_method_once()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+
+            // Act
+            service.DeleteLot(id);
+
+            // Assert
+            uow.Received(1).Commit();
+        }
+
+        [Test]
+        public void GetLot_should_return_exception_if_id_is_less_or_equals_0()
+        {
+            // Arrange
+            int id = -1;
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.GetLot(id); });
+        }
+
+        [Test]
+        public void GetLot_should_return_exception_if_lot_equals_null()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            uow.Lots.Get(id).Returns((Lot)null);
+
+            // Act and assert
+            Assert.Throws<InvalidIdException>(delegate { service.GetLot(id); });
+        }
+
+        [Test]
+        public void GetLot_should_return_LotDTO()
+        {
+            // Arrange
+            int id = fixture.Create<int>();
+            Lot lot = new Lot();
+            LotDTO lotDTO = mapper.Map<Lot, LotDTO>(lot);
+            uow.Lots.Get(id).Returns(lot);
+
+            // Act
+            var testLotDTO = service.GetLot(id);
+
+            // Assert
+            Assert.IsTrue(testLotDTO.GetType() == lotDTO.GetType());
+        }
+
+        [Test]
+        public void GetLot_should_call_lot_repository_get_method_once()
+        {
+            // Arrange
+            int id = 1;
+            uow.Lots.Get(id).Returns(new Lot());
+
+            // Act
+            service.GetLot(id);
+
+            // Assert
+            uow.Lots.Received(1).Get(Arg.Any<int>());
+        }
+
+        [Test]
+        public void GetLots_should_call_lot_repository_get_all_method_once()
+        {
+            // Arrange
+
+            // Act
+            service.GetLots();
+
+            // Assert
+            uow.Lots.Received(1).GetAll();
+        }
+
+        [Test]
+        public void GetLots_should_return_IEnumerable_LotDTO()
+        {
+            // Arrange
+            IEnumerable<LotDTO> lotDTOs = new List<LotDTO>();
+
+            // Act
+            var testLotDTOs = service.GetLots();
+
+            // Assert
+            Assert.IsTrue(testLotDTOs.GetType() == lotDTOs.GetType());
+        }
+
+        [Test]
+        public void GetLotsByCategory_should_return_exception_if_category_equals_null()
+        {
+            // Arrange
+            string category = null;
+
+            // Act and assert
+            Assert.Throws<InvalidCategoryException>(delegate { service.GetLotsByCategory(category); });
+        }
+
+        [Test]
+        public void GetLotsByCategory_should_return_IEnumerable_LotDTO()
+        {
+            // Arrange
+            string category = fixture.Create<string>();
+            IEnumerable<LotDTO> lotDTOs = new List<LotDTO>();
+
+            // Act
+            var testLotDTOs = service.GetLotsByCategory(category);
+
+            // Assert
+            Assert.IsTrue(testLotDTOs.GetType() == lotDTOs.GetType());
+        }
+
+        [Test]
+        public void GetLotsByName_should_return_exception_if_name_equals_null()
+        {
+            // Arrange
+            string name = null;
+
+            // Act and assert
+            Assert.Throws<InvalidNameException>(delegate { service.GetLotsByName(name); });
+        }
+
+        [Test]
+        public void GetLotsByName_should_return_IEnumerable_LotDTO()
+        {
+            // Arrange
+            string name = fixture.Create<string>();
+            IEnumerable<LotDTO> lotDTOs = new List<LotDTO>();
+
+            // Act
+            var testLotDTOs = service.GetLotsByName(name);
+
+            // Assert
+            Assert.IsTrue(testLotDTOs.GetType() == lotDTOs.GetType());
+        }
+
+        [Test]
+        public void GetNotSoldLots_should_return_IEnumerable_LotDTO()
+        {
+            // Arrange
+            IEnumerable<LotDTO> lotDTOs = new List<LotDTO>();
+
+            // Act
+            var testLotDTOs = service.GetNotSoldLots();
+
+            // Assert
+            Assert.IsTrue(testLotDTOs.GetType() == lotDTOs.GetType());
+        }
+
+        [Test]
+        public void GetNotSoldLots_should_call_lot_repository_get_not_sold_lots_method_once()
+        {
+            // Arrange
+
+            // Act
+            service.GetNotSoldLots();
+
+            // Assert
+            uow.Lots.Received(1).GetNotSoldLots();
+        }
+
+        [Test]
+        public void GetSoldLots_should_return_IEnumerable_LotDTO()
+        {
+            // Arrange
+            IEnumerable<LotDTO> lotDTOs = new List<LotDTO>();
+
+            // Act
+            var testLotDTOs = service.GetSoldLots();
+
+            // Assert
+            Assert.IsTrue(testLotDTOs.GetType() == lotDTOs.GetType());
+        }
+
+        [Test]
+        public void GetSoldLots_should_call_lot_repository_get_sold_lots_method_once()
+        {
+            // Arrange
+
+            // Act
+            service.GetSoldLots();
+
+            // Assert
+            uow.Lots.Received(1).GetSoldLots();
+        }
+
+        [Test]
+        public void UpdateLot_should_return_exception_if_lot_equals_null()
+        {
+            // Arrange
+            LotDTO lot = null;
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.UpdateLot(lot); });
+        }
+
+        [Test]
+        public void UpdateLot_should_return_exception_if_lot_name_equals_null()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Name = null };
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.UpdateLot(lot); });
+        }
+
+        [Test]
+        public void UpdateLot_should_return_exception_if_lot_owner_equals_null()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Owner = null};
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.UpdateLot(lot); });
+        }
+
+        [Test]
+        public void UpdateLot_should_return_exception_if_lot_category_equals_null()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Category = null };
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.UpdateLot(lot); });
+        }
+
+        [Test]
+        public void UpdateLot_should_return_exception_if_lot_not_exist_in_db()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO();
+            uow.Lots.Get(lot.ID).Returns((Lot)null);
+
+            // Act and assert
+            Assert.Throws<InvalidLotException>(delegate { service.UpdateLot(lot); });
+        }
+
+        [Test]
+        public void UpdateLot_should_call_lot_repository_get_method_once()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { Name = "a", Owner = "b", Category = "c" };
+            uow.Lots.Get(lot.ID).Returns(mapper.Map<LotDTO, Lot>(lot));
+
+            // Act
+            service.UpdateLot(lot);
+
+            // Assert
+            uow.Lots.Received(1).Get(Arg.Any<int>());
+        }
+
+        [Test]
+        public void UpdateLot_should_call_lot_repository_update_method_once()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { ID = 1, Name = "a", Owner = "b", Category = "c" };
+
+            // Act
+            service.UpdateLot(lot);
+
+            // Assert
+            uow.Lots.Received(1).Update(Arg.Any<Lot>());
+        }
+
+        [Test]
+        public void UpdateLot_should_call_unit_of_work_commit_method_once()
+        {
+            // Arrange
+            LotDTO lot = new LotDTO { ID = 1, Name = "a", Owner = "b", Category = "c" };
+
+            // Act
+            service.UpdateLot(lot);
+
+            // Assert
+            uow.Received(1).Commit();
+        }
+    }
+}
